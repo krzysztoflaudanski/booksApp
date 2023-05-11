9db6629860e462aa395eea42cb0521ab5d0bf6b5
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-05-11 20:16:44 +0200

    add determineRatingBgc script.js

--- src/js/script.js
+++ src/js/script.js
@@ -8,11 +8,13 @@ const select = {
     },
     books: {
         imageWrapper: '.books-list .book__image',
+        list: '.books-list li'
     }
 };
 
 const templates = {
     product: Handlebars.compile(document.querySelector(select.templateOf.product).innerHTML)
+
 };
 
 const data = dataSource;
@@ -29,6 +31,14 @@ function render() {
 
         productData = (productData, data.books[productData]);
         //console.log(productData);
+
+        const ratingBgc = determineRatingBgc(productData.rating);
+        
+        const ratingWidth = productData.rating * 10;
+       
+        productData.ratingBgc = ratingBgc;
+
+        productData.ratingWidth = ratingWidth;
 
         const generatedHTML = templates.product(productData);
         //console.log(generatedHTML);
@@ -96,9 +106,67 @@ function initActions() {
                 filters.splice(indexOfFilters, 1);
             }
         }
-        console.log(filters);
-});
+        filterBooks();
+        //console.log(filters);
+    });
 }
+
+function filterBooks() {
+    for (let book of data.books) {
+
+        let shouldBeHidden = false;
+
+        for (let filter in filters) {
+
+            filter = filters[filter];
+
+            if (book.details[filter] != true) {
+
+                shouldBeHidden = true;
+
+                break;
+            }
+        }
+        const showBook = document.querySelector('.book__image[data-id="' + book.id + '"]');
+
+        if (shouldBeHidden == false) {
+
+            showBook.classList.remove('hidden');
+
+        } else {
+
+            showBook.classList.add('hidden');
+        }
+
+    }
+}
+
+function determineRatingBgc(rating) {
+
+    let ratingBgc = '';
+
+    if (rating < 6) {
+
+        ratingBgc = 'linear-gradient(to bottom. #fefcea 0%, #f1da36 100%';
+
+    } else if (rating > 6 && rating <= 8) {
+
+        ratingBgc = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%';
+
+    } else if (rating > 8 && rating <= 9) {
+
+        ratingBgc = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%';
+
+    } else if (rating > 9) {
+
+        ratingBgc = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%';
+    }
+    return ratingBgc;
+}
+
+
+
+
 
 render();
 
